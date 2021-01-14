72483ed907b8926334ddad14b3f444a6dc37fc44
LavanMoogagit <lavanmooga@gmail.com>
2021-01-14 16:13:30 +0530

    Adding Resposiveness and javascript btn event listener

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script>
+                      
+ 
+document.getElementById("MyBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+    alert ("Thank You -:) !");
+  }                    
+                      </script>
+
+
